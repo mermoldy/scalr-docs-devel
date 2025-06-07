bd43e51084c46f5507f5cdbc7a5030efa925079b
Serhii Babak <s.babak@scalr.com>
2025-06-07 21:06:03 +0300

    Updates

--- assets/js/53d8affa.4a4d9b45.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[409],{4006:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/scalr-docs-devel/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.a700988b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/scalr-docs-devel/blog","blogTitle":"Blog","authorsListPath":"/scalr-docs-devel/blog/authors"}')}}]);
